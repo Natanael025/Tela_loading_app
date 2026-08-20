79ad630231b45a993634fb20e55d5d14e3908991
Natanael025 <natanvieira2501@gmail.com>
2026-08-20 20:31:52 -0300

    Change title and paragraph styles in App component
    Updated text content and styles in App.jsx.

--- App.jsx
+++ App.jsx
@@ -10,11 +10,11 @@ export default function App() {
         style={styles.logo}
         source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7C0NlbHXsMpd67_3fP8sauaf7Xy2cDkCNMVcN6imz158sgpE0eFyx_5xK&s=10'}}
         />
-        <Text style={styles.title}>Centro Ninja</Text>
-        <Text style={styles.paragraph}>Seu lugar para se tornar um ninja</Text>
+        <Text style={styles.title}>Ninja Dojo</Text>
+        <Text style={styles.paragraph1}>Onde grandes mestres são moldados...</Text>
       </View>
       <View style={styles.footer}>
-        <Text style={styles.paragraph}>v1.0 - Natanael Vieira</Text>
+        <Text style={styles.paragraph2}>v1.0 - Natanael Vieira</Text>
       </View>
     </View>
   );
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#0f0f14',
   },
   body: {
     flex:6,
@@ -33,19 +33,26 @@ const styles = StyleSheet.create({
   footer: {
     flex:1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    borderTopWidth: 2,
+    borderColor: 'gray'
   },
   logo: {
     width: 70,
     height: 70,
-    borderRadius: 10
+    borderRadius: 10,
+    marginBottom: 10
   },
   title: {
     color: 'white',
     fontSize: 40,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginBottom: 10
   },
-  paragraph: {
-    color: 'white',
+  paragraph1: {
+    color: '#fff',
+  },
+  paragraph2: {
+    color: '#888',
   }
 });
